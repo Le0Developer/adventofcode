1071989c0e89b7b818f83f95e1a9f4bc0e3b13e0
Leo Developer <leodeveloper@protonmail.com>
2020-12-16 11:20:19 +0100

    [day 02] fix tests

--- day02/day2_test.v
+++ day02/day2_test.v
@@ -1,8 +1,8 @@
 module main
 
 fn test_parse() {
-	policies = parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'])
-	policy = policies[0]
+	policies := parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'])
+	policy := policies[0]
 	assert policy.value_0 == 1
 	assert policy.value_1 == 3
 	assert policy.char == `a`
@@ -10,9 +10,11 @@ fn test_parse() {
 }
 
 fn test_solve_a() {
-	assert solve_a(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']) == 2
+	policies := parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'])
+	assert solve_a(policies) == 2
 }
 
 fn test_solve_b() {
-	assert solve_b(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']) == 1
+	policies := parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'])
+	assert solve_b(policies) == 1
 }
