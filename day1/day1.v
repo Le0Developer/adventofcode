51fd0151b628b2f16dc536f0c537398e5392c1b8
Leo Developer <leodeveloper@protonmail.com>
2020-12-16 09:56:58 +0100

    [day 1] day saved

---
+++ day1/day1.v
@@ -0,0 +1,45 @@
+module main
+
+import os
+import os.cmdline
+
+fn solve_a(entries []int) ?int {
+	for value in entries {
+		if 2020 - value in entries {
+			return (2020 - value) * value
+		}
+	}
+	return error('No solution found')
+}
+
+fn solve_b(entries []int) ?int {
+	for i in 0 .. entries.len {
+		value := entries[i]
+		for k in (i + 1) .. entries.len {
+			valuek := entries[k]
+			if 2020 - value - valuek in entries {
+				return (2020 - value - valuek) * value * valuek
+			}
+		}
+	}
+	return error('No solution found')
+}
+
+fn main() {
+	input_file := os.args[1]
+	input := os.read_lines(input_file) ?
+	entries := input.map(it.int())
+	mut solution := 0
+	if '-b' in cmdline.only_options(os.args) {
+		solution = solve_b(entries) or {
+			println(err)
+			return
+		}
+	} else {
+		solution = solve_a(entries) or {
+			println(err)
+			return
+		}
+	}
+	println('Solution is: $solution')
+}
---
+++ day1/day1_test.v
@@ -0,0 +1,33 @@
+module main
+
+fn test_solve_a() {
+	solution := solve_a([1721, 979, 366, 299, 675, 1456]) or {
+		assert false
+		return
+	}
+	assert solution == 514579
+}
+
+fn test_solve_b() {
+	solution := solve_b([1721, 979, 366, 299, 675, 1456]) or {
+		assert false
+		return
+	}
+	assert solution == 241861950
+}
+
+fn test_badsolve_a() {
+	solve_a([0, 1, 2, 3, 4, 5, 6, 7]) or {
+		assert err == 'No solution found'
+		return
+	}
+	assert false
+}
+
+fn test_badsolve_b() {
+	solve_b([0, 1, 2, 3, 4, 5, 6, 7]) or {
+		assert err == 'No solution found'
+		return
+	}
+	assert false
+}
