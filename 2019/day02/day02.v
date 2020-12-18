5715deb76111b25b0b810a19bb4d3a72cc92a3ff
Leo Developer <leodeveloper@protonmail.com>
2020-12-18 23:05:16 +0100

    [2019/02] magic smoke solved

---
+++ 2019/day02/day02.v
@@ -0,0 +1,58 @@
+module main
+
+import os
+import os.cmdline
+
+fn solve_a(input []string, noun int, verb int) int {
+	mut mem := input[0].split(',').map(it.int())
+	if noun >= 0 {
+		mem[1] = noun
+	}
+	if verb >= 0 {
+		mem[2] = verb
+	}
+	mut pointer := 0
+	for {
+		instr := mem[pointer]
+		if instr == 1 {
+			source1 := mem[pointer + 1]
+			source2 := mem[pointer + 2]
+			dest := mem[pointer + 3]
+			mem[dest] = mem[source1] + mem[source2]
+			pointer += 4
+		} else if instr == 2 {
+			source1 := mem[pointer + 1]
+			source2 := mem[pointer + 2]
+			dest := mem[pointer + 3]
+			mem[dest] = mem[source1] * mem[source2]
+			pointer += 4
+		} else if instr == 99 {
+			break
+		}
+	}
+	return mem[0]
+}
+
+fn solve_b(input []string) int {
+	for noun in 0..99 {
+		for verb in 0..99 {
+			result := solve_a(input, noun, verb)
+			if result == 19690720 {
+				return 100 * noun + verb
+			}
+		}
+	}
+	return -1
+}
+
+fn main() {
+	input_file := os.args[1]
+	input := os.read_lines(input_file) ?
+	mut solution := 0
+	if '-b' in cmdline.only_options(os.args) {
+		solution = solve_b(input)
+	} else {
+		solution = solve_a(input, 12, 2)
+	}
+	println('Solution is: $solution')
+}
---
+++ 2019/day02/day02_test.v
@@ -0,0 +1,10 @@
+module main
+
+fn test_solve_a() {
+	input := ['1,9,10,3,2,3,11,0,99,30,40,50']
+	assert solve_a(input, -1, -1) == 3500
+}
+
+// Not possible to test solve_b() without actual test data
+// which I'm not gonna make public...
+// Feel free to PR
