e9adea0a1ba1fab64368d64c5ae58793478aee70
Leo Developer <leodeveloper@protonmail.com>
2020-12-18 22:48:20 +0100

    [2019/01] spacecraft fuel problem solved

---
+++ 2019/day01/day01.v
@@ -0,0 +1,36 @@
+module main
+
+import os
+import os.cmdline
+
+fn solve_a(input []string) int {
+	mut total := 0
+	for line in input {
+		total += line.int() / 3 - 2
+	}
+	return total
+}
+
+fn solve_b(input []string) int {
+	mut total := 0
+	for line in input {
+		mut fuel := line.int() / 3 - 2
+		for fuel > 0 {
+			total += fuel
+			fuel = fuel / 3 - 2
+		}
+	}
+	return total
+}
+
+fn main() {
+	input_file := os.args[1]
+	input := os.read_lines(input_file) ?
+	mut solution := 0
+	if '-b' in cmdline.only_options(os.args) {
+		solution = solve_b(input)
+	} else {
+		solution = solve_a(input)
+	}
+	println('Solution is: $solution')
+}
---
+++ 2019/day01/day01_test.v
@@ -0,0 +1,11 @@
+module main
+
+fn test_solve_a() {
+	inputs := ['12', '14', '1969', '100756']
+	assert solve_a(inputs) == 34241
+}
+
+fn test_solve_b() {
+	inputs := ['12', '14', '1969', '100756']
+	assert solve_b(inputs) == 51316
+}
