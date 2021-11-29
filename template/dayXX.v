583dce8ab07518a4d67048a8c5b029f57656604e
Leo Developer <leodeveloper@protonmail.com>
2021-11-29 23:51:43 +0100

    prepare for 2021

---
+++ template/dayXX.v
@@ -0,0 +1,28 @@
+import flag
+import os
+
+fn challenge_a(input_lines []string) i64 {
+	// code here
+}
+
+fn challenge_b(input_lines []string) i64 {
+	// code here
+}
+
+fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application('AdventOfCode 202_ day __')
+	fp.version('v0.1.0')
+	fp.limit_free_args_to_exactly(1)
+	is_b := fp.bool('b', `b`, false, 'b challenge')
+	// more options here
+	additional_args := fp.finalize() or {
+		eprintln(err)
+		println(fp.usage())
+		return
+	}
+	input_filename := additional_args[0]
+	input_lines := os.read_lines(input_filename) ?
+	solution := if !is_b { challenge_a(input_lines) } else { challenge_b(input_lines) }
+	println('Solution is $solution')
+}
---
+++ template/dayXX_test.v
@@ -0,0 +1,10 @@
+
+fn test_day_XX_a() {
+	input_lines := []
+	assert challenge_a(input_lines) == 0
+}
+
+fn test_day_XX_b() {
+	input_lines := []
+	assert challenge_b(input_lines) == 0
+}
