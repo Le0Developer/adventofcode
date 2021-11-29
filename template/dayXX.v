3a26ca8aad709788d4e7f36bae880c229c937c94
Leo Developer <leodeveloper@protonmail.com>
2021-11-30 00:04:55 +0100

    update template

--- template/dayXX.v
+++ template/dayXX.v
@@ -1,19 +1,23 @@
+module main
+
 import flag
 import os
 
 fn challenge_a(input_lines []string) ?i64 {
 	// code here
+	return -1
 }
 
 fn challenge_b(input_lines []string) ?i64 {
 	// code here
+	return -1
 }
 
 fn main() {
 	mut fp := flag.new_flag_parser(os.args)
 	fp.application('AdventOfCode 202_ day __')
 	fp.version('v0.1.0')
-	fp.limit_free_args_to_exactly(1)
+	fp.limit_free_args_to_exactly(1) ?
 	is_b := fp.bool('b', `b`, false, 'b challenge')
 	// more options here
 	additional_args := fp.finalize() or {
--- template/dayXX_test.v
+++ template/dayXX_test.v
@@ -1,10 +1,11 @@
+module main
 
-fn test_day_XX_a() {
-	input_lines := []
-	assert challenge_a(input_lines) == 0
+fn test_challenge_a() ? {
+	input_lines := ['']
+	assert (challenge_a(input_lines) ?) == -1
 }
 
-fn test_day_XX_b() {
-	input_lines := []
-	assert challenge_b(input_lines) == 0
+fn test_challenge_b() ? {
+	input_lines := ['']
+	assert (challenge_b(input_lines) ?) == -1
 }
