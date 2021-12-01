72e77895f2a7aa4ff21228e5506e55014f3196a4
Leo Developer <leodeveloper@protonmail.com>
2021-12-01 14:22:35 +0100

    [2019/05] add day 05

---
+++ 2019/day05/day05.v
@@ -0,0 +1,169 @@
+module main
+
+import flag
+import os
+
+enum ParamterMode {
+	position
+	immediate
+}
+
+fn challenge_a(input_lines []string) ?i64 {
+	mut mem := input_lines[0].split(',').map(it.int())
+	mut pointer := 0
+	mut last_out := -1
+	for {
+		instr := mem[pointer]
+		instrs := instr.str()
+		param_0 := if instrs.len >= 3 && instrs[instrs.len - 3] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		param_1 := if instrs.len >= 4 && instrs[instrs.len - 4] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		// param_2 := if instrs.len >= 5 && instrs[instrs.len - 5] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		match (instr % 100) {
+			1 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = value1 + value2
+				pointer += 4
+			}
+			2 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = value1 * value2
+				pointer += 4
+			}
+			3 {
+				dest := mem[pointer + 1]
+				mem[dest] = 1
+				pointer += 2
+			}
+			4 {
+				source := mem[pointer + 1]
+				last_out = mem[source]
+				pointer += 2
+			}
+			99 {
+				break
+			}
+			else {
+				return error("invalid instruction: $instr at $pointer")
+			}
+		}
+	}
+	return last_out
+}
+
+fn challenge_b(input_lines []string, input int) ?i64 {
+	mut mem := input_lines[0].split(',').map(it.int())
+	mut pointer := 0
+	mut last_out := -1
+	for {
+		instr := mem[pointer]
+		instrs := instr.str()
+		param_0 := if instrs.len >= 3 && instrs[instrs.len - 3] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		param_1 := if instrs.len >= 4 && instrs[instrs.len - 4] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		// param_2 := if instrs.len >= 5 && instrs[instrs.len - 5] == `1` { ParamterMode.immediate } else { ParamterMode.position }
+		match (instr % 100) {
+			1 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = value1 + value2
+				pointer += 4
+			}
+			2 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = value1 * value2
+				pointer += 4
+			}
+			3 {
+				dest := mem[pointer + 1]
+				mem[dest] = input
+				pointer += 2
+			}
+			4 {
+				source1 := mem[pointer + 1]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				last_out = value1
+				pointer += 2
+			}
+			5 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				if value1 != 0 {
+					pointer = value2
+				} else {
+					pointer += 3
+				}
+			}
+			6 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				if value1 == 0 {
+					pointer = value2
+				} else {
+					pointer += 3
+				}
+			}
+			7 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = int(value1 < value2)
+				pointer += 4
+			}
+			8 {
+				source1 := mem[pointer + 1]
+				source2 := mem[pointer + 2]
+				dest := mem[pointer + 3]
+				value1 := if param_0 == .immediate { source1 } else { mem[source1] }
+				value2 := if param_1 == .immediate { source2 } else { mem[source2] }
+				mem[dest] = int(value1 == value2)
+				pointer += 4
+			}
+			99 {
+				break
+			}
+			else {
+				return error("invalid instruction: $instr at $pointer")
+			}
+		}
+	}
+	return last_out
+}
+
+fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application('AdventOfCode 2019 day 05')
+	fp.version('v0.1.0')
+	fp.skip_executable()
+	fp.limit_free_args_to_exactly(1) ?
+	is_b := fp.bool('b', `b`, false, 'b challenge')
+	// more options here
+	additional_args := fp.finalize() or {
+		eprintln(err)
+		println(fp.usage())
+		return
+	}
+	input_filename := additional_args[0]
+	input_lines := os.read_lines(input_filename) ?
+	solution := if !is_b { challenge_a(input_lines) ? } else { challenge_b(input_lines, 5) ? }
+	println('Solution is $solution')
+}
---
+++ 2019/day05/day05_test.v
@@ -0,0 +1,34 @@
+module main
+
+fn test_challenge_a() ? {
+	input_lines := ['1002,4,3,4,33']
+	assert (challenge_a(input_lines) ?) == -1
+}
+
+struct TestData {
+	input  string
+	input_0 int
+	input_1 int
+	input_8 int
+	input_9 int
+}
+
+fn test_challenge_b() ? {
+	test_data := [
+		TestData{"3,9,8,9,10,9,4,9,99,-1,8", 0, 0, 1, 0},
+		TestData{"3,9,7,9,10,9,4,9,99,-1,8", 1, 1, 0, 0},
+		TestData{"3,3,1108,-1,8,3,4,3,99", 0, 0, 1, 0},
+		TestData{"3,3,1107,-1,8,3,4,3,99", 1, 1, 0, 0},
+		TestData{"3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 0, 1, 1, 1},
+		TestData{"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 0, 1, 1, 1},
+		TestData{"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 999, 999, 1000, 1001}
+	]
+	for test in test_data {
+		println(test)
+		input_lines := [test.input]
+		assert (challenge_b(input_lines, 0) ?) == test.input_0
+		assert (challenge_b(input_lines, 1) ?) == test.input_1
+		assert (challenge_b(input_lines, 8) ?) == test.input_8
+		assert (challenge_b(input_lines, 9) ?) == test.input_9
+	}
+}
