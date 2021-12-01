684ac36e938e6a0447b1053c8993be9a2b73f210
Leo Developer <leodeveloper@protonmail.com>
2021-12-01 13:05:04 +0100

    fmt + fix new year readme setup

--- template/setup.v
+++ template/setup.v
@@ -1,7 +1,5 @@
-
 import flag
 import os
-
 
 fn main() {
 	mut fp := flag.new_flag_parser(os.args)
@@ -17,11 +15,11 @@ fn main() {
 	day := additional_args[1]
 
 	if year.len != 4 {
-		eprintln("invalid year")
+		eprintln('invalid year')
 		return
 	}
 	if day.len != 2 {
-		eprintln("invalid day")
+		eprintln('invalid day')
 		return
 	}
 
@@ -38,31 +36,31 @@ fn main() {
 	day_xx_test := ($embed_file('template/dayXX_test.v')).to_string()
 	os.write_file('$year/day$day/day${day}_test.v', day_xx_test) ?
 
-	mut readme := os.read_lines("README.md") ?
+	mut readme := os.read_lines('README.md') ?
 	mut table_start := -1
 	mut found := false
 	for i, line in readme {
-		if line.starts_with("|  $year  |") {
+		if line.starts_with('|  $year  |') {
 			mut pretty_day := day
 			if pretty_day[0] == `0` {
-				pretty_day = " " + pretty_day[1..2]
+				pretty_day = ' ' + pretty_day[1..2]
 			}
-			readme[i] = "|  $year  |    $pretty_day/25    |   V           |"
+			readme[i] = '|  $year  |    $pretty_day/25    |   V           |'
 			found = true
 			break
 		}
-		if line == "|  Year  |  Progress   |  Language(s)  |" {
+		if line == '|  Year  |  Progress   |  Language(s)  |' {
 			table_start = i
 		}
 	}
 	if !found {
 		mut pretty_day := day
 		if pretty_day[0] == `0` {
-			pretty_day = " " + pretty_day[1..2]
+			pretty_day = ' ' + pretty_day[1..2]
 		}
-		readme.insert(table_start + 1, "|  $year  |    $pretty_day/25    |   V           |")
+		readme.insert(table_start + 2, '|  $year  |    $pretty_day/25    |   V           |')
 	}
 
-	readme << ""  // read_lines removes final newline
-	os.write_file("README.md", readme.join_lines()) ?
+	readme << '' // read_lines removes final newline
+	os.write_file('README.md', readme.join_lines()) ?
 }
