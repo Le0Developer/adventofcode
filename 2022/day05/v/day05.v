a9e39229ea08ebd9097136d7dfe8c677fc346625
Leo Developer <leodeveloper@protonmail.com>
2022-12-18 00:26:04 +0100

    [2022/05] add day

---
+++ 2022/day05/v/day05.v
@@ -0,0 +1,94 @@
+module main
+
+import flag
+import os
+
+fn challenge_a(input_lines []string) ?string {
+	mut stacks := [][]string{}
+	mut initial_stacking := true
+	for line in input_lines {
+		if line == ''  {
+			initial_stacking = false
+		}
+		else if initial_stacking {
+			for i := 1; i < line.len; i += 4 {
+				idx := (i - 1) / 4
+				for idx >= stacks.len {
+					stacks << []string{}
+				}
+				if line[i] != ` ` && line[i] > 0x40 {
+					stacks[idx].insert(0, line[i].ascii_str())
+				}
+			}
+		} else {
+			instructions := line.split(' ')
+			amount := instructions[1].int()
+			source := instructions[3].int()
+			destination := instructions[5].int()
+			
+			for _ in 0 .. amount {
+				stacks[destination - 1] << stacks[source - 1].pop()
+			}
+		}
+	}
+	mut res := ""
+	for stack in stacks {
+		res += stack.last()
+	}
+	return res
+}
+
+fn challenge_b(input_lines []string) ?string {
+	mut stacks := [][]string{}
+	mut initial_stacking := true
+	for line in input_lines {
+		if line == ''  {
+			initial_stacking = false
+		}
+		else if initial_stacking {
+			for i := 1; i < line.len; i += 4 {
+				idx := (i - 1) / 4
+				for idx >= stacks.len {
+					stacks << []string{}
+				}
+				if line[i] != ` ` && line[i] > 0x40 {
+					stacks[idx].insert(0, line[i].ascii_str())
+				}
+			}
+		} else {
+			instructions := line.split(' ')
+			amount := instructions[1].int()
+			source := instructions[3].int()
+			destination := instructions[5].int()
+			
+			current := stacks[destination - 1].len
+			for _ in 0 .. amount {
+				stacks[destination - 1].insert(current, stacks[source - 1].pop())
+			}
+		}
+	}
+	mut res := ""
+	for stack in stacks {
+		res += stack.last()
+	}
+	return res
+}
+
+fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application('AdventOfCode 2022 day 05')
+	fp.version('v0.1.0')
+	fp.skip_executable()
+	fp.limit_free_args_to_exactly(1)!
+	is_b := fp.bool('b', `b`, false, 'b challenge')
+	// more options here
+	additional_args := fp.finalize() or {
+		eprintln(err)
+		println(fp.usage())
+		return
+	}
+	input_filename := additional_args[0]
+	input_lines := os.read_lines(input_filename)!
+	solution := if !is_b { challenge_a(input_lines)? } else { challenge_b(input_lines)? }
+	println('Solution is ${solution}')
+}
---
+++ 2022/day05/v/day05_test.v
@@ -0,0 +1,11 @@
+module main
+
+fn test_challenge_a() ? {
+	input_lines := ['    [D]    ', '[N] [C]    ', '[Z] [M] [P]', ' 1   2   3 ', '', 'move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
+	assert (challenge_a(input_lines)?) == "CMZ"
+}
+
+fn test_challenge_b() ? {
+	input_lines := ['    [D]    ', '[N] [C]    ', '[Z] [M] [P]', ' 1   2   3 ', '', 'move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
+	assert (challenge_b(input_lines)?) == "MCD"
+}
